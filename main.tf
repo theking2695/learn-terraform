2870e08cf90a1d19a7b85763cfcfaf3074f31dd7
Jacob Congdon <jacobcongdon234@gmail.com>
2025-04-14 10:38:03 -0400

    Remove cloud block

--- main.tf
+++ main.tf
@@ -5,24 +5,24 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+/*  data "aws_ami" "ubuntu" {
+    most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+    filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+    }
 
-  owners = ["099720109477"] # Canonical
-}
+    owners = ["099720109477"] # Canonical
+  }*/
 
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-084568db4383264d4"
   instance_type = var.instance_type
 
   tags = {
--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /*cloud {
+    organization = "Learn-Terraform-2695"
 
     workspaces {
       name = "learn-terraform"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
